883de23818cc9590f9c240d6266b4e4b846ab5be
BOISE\Stephanie_Labastida <slabastida@cityofboise.org>
2018-11-08 15:49:45 -0700

    attempts

---
+++ .vs/MouseoverPractice/action.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+var perf = document.getElementById("perfect");
+console.log(perf);
+// var great = document.getElementById("great");
+// var good = document.getElementById("good");
+// var fine = document.getElementById("fine");
+// var okay = document.getElementById("okay");
+// var notGood = document.getElementById("notGood");
+// var bad = document.getElementById("bad");
+// var hurts = document.getElementById("hurts");
+// var inPain = document.getElementById("inPain");
+// var excrutiating = document.getElementById("excrutiating");
+
+// var faces = [perf, great, good, fine, okay,
+//                 notGood, bad, hurts, inPain, excrutiating];
+// var facesLength = faces.length;
+
+// perf.onmouseover = function(){
+//     assignMessage();
+// }
+// // for(var i = 0; i < facesLength; i++){
+// //     console.log(perf);
+// //     faces[i].onmouseover = function(){
+// //         assignMessage();
+// //     }
+// // }
+
+// function assignMessage(){
+//     var message = document.getElementById("message");
+//     message.innerHTML = 'Test';
+
+// }
